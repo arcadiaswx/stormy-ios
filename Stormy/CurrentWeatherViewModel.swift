9934a48bb049decb13005cb7fd70c5df832d3c10
Craig Booker <info@itech.plus>
2018-07-25 22:14:43 -0500

    Added modelView for current weather.

---
+++ Stormy/CurrentWeatherViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentWeatherViewModel.swift
+//  Stormy
+//
+//  Created by Craig on 7/25/18.
+//  Copyright © 2018 ITECH PLUS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeatherViewModel {
+    
+    
+}
