5619fc6145010452a58ed9e501998bac762a2082
Craig Booker <info@itech.plus>
2018-07-26 11:08:56 -0500

    Hooked up the main view to the viewController.

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
