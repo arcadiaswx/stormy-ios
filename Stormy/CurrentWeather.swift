b123008942d43aadf50e966a0fb5c0aba2447f93
Craig Booker <info@itech.plus>
2018-07-25 22:06:00 -0500

    Added image assets and defined the CurrentWeather struct/extension.

--- Stormy/CurrentWeather.swift
+++ Stormy/CurrentWeather.swift
@@ -16,3 +16,21 @@ struct CurrentWeather {
     let summary: String
     let icon: String
 }
+
+extension CurrentWeather {
+    var iconImage: UIImage {
+        switch icon {
+        case "clear-day": return #imageLiteral(resourceName: "clear-day")
+        case "clear-night": return #imageLiteral(resourceName: "clear-night")
+        case "rain": return #imageLiteral(resourceName: "rain")
+        case "snow": return #imageLiteral(resourceName: "snow")
+        case "sleet": return #imageLiteral(resourceName: "sleet")
+        case "wind": return #imageLiteral(resourceName: "wind")
+        case "fog": return #imageLiteral(resourceName: "fog")
+        case "cloudy": return #imageLiteral(resourceName: "cloudy")
+        case "partly-cloudy-day": return #imageLiteral(resourceName: "partly-cloudy")
+        case "partly-cloudy-night": return #imageLiteral(resourceName: "partly-cloudy-night")
+        default: return #imageLiteral(resourceName: "default")
+        }
+    }
+}
