0ed0fc8537dbab1bd6d381a7e9513e227a6445ae
Craig Booker <info@itech.plus>
2018-07-25 21:48:06 -0500

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Craig on 7/25/18.
+//  Copyright © 2018 ITECH PLUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
