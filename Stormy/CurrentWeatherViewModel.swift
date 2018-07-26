8f63cad062143ccca25b25f95eba487c875f8f06
Craig Booker <info@itech.plus>
2018-07-26 10:06:00 -0500

    Flushed out the initializer for the struct CurrentWeatherViewModel.

--- Stormy/CurrentWeatherViewModel.swift
+++ Stormy/CurrentWeatherViewModel.swift
@@ -16,4 +16,18 @@ struct CurrentWeatherViewModel {
     let summary: String
     let icon: UIImage
     
+    
+    // Add basic initializer for the struct.
+    init(model: CurrentWeather) {
+        let roundedTemperature = Int(model.temperature)
+        self.temperature = "\(roundedTemperature)º"
+        let humidityPercentValue = Int(model.humidity * 100)
+        self.humidity = "\(humidityPercentValue)%"
+        let precipitationPercentValue = Int(model.precipProbability * 100)
+        self.precipitationProbability = "\(precipitationPercentValue)%"
+        
+        self.summary = model.summary
+        self.icon = model.iconImage
+        
+    }
 }
